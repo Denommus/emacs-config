25bdcba0c7f7353ad6a71ef13468d83807566c45
Yuri Albuquerque <yuridenommus@gmail.com>
2012-12-30 16:10:27 -0400

    Adding rbenv to emacs' PATH.

--- init.el
+++ init.el
@@ -46,6 +46,8 @@
 
 ;; Ruby
 (add-hook 'ruby-mode-hook 'zossima-mode)
+(setenv "PATH" (concat (getenv "HOME") "/.rbenv/shims:" (getenv "HOME") "/.rbenv/bin:" (getenv "PATH")))
+(setq exec-path (cons (concat (getenv "HOME") "/.rbenv/shims") (cons (concat (getenv "HOME") "/.rbenv/bin") exec-path)))
 
 ;; ERC + Tor
 (setq socks-override-functions nil)
