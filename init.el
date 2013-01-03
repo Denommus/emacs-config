801a9cdfb28d11a2715710f31b36a439427b39ed
Yuri Albuquerque <yuridenommus@gmail.com>
2013-01-03 09:47:37 -0400

    Adding global-rinari-mode call.
    In future versions, rinari won't call this function automatically, so I
    need to add the call before it is updated.

--- init.el
+++ init.el
@@ -49,6 +49,7 @@
 (add-hook 'ruby-mode-hook 'zossima-mode)
 (setenv "PATH" (concat (getenv "HOME") "/.rbenv/shims:" (getenv "HOME") "/.rbenv/bin:" (getenv "PATH")))
 (setq exec-path (cons (concat (getenv "HOME") "/.rbenv/shims") (cons (concat (getenv "HOME") "/.rbenv/bin") exec-path)))
+(global-rinari-mode)
 
 ;; ERC + Tor
 (setq socks-override-functions nil)
