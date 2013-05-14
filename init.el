fb4ad9305f64e62e8ea3bffa651e433af945b478
Yuri Albuquerque <yuridenommus@gmail.com>
2013-05-14 18:03:38 -0400

    ParEdit in SLIME REPL.

--- init.el
+++ init.el
@@ -226,6 +226,11 @@
      (add-hook 'lisp-mode-hook             #'enable-paredit-mode)
      (add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
      (add-hook 'scheme-mode-hook           #'enable-paredit-mode)
+     (add-hook 'slime-repl-mode-hook #'(lambda () (paredit-mode +1)))
+     (defun override-slime-repl-bindings-with-paredit ()
+       (define-key slime-repl-mode-map
+         (read-kbd-macro paredit-backward-delete-key) nil))
+     (add-hook 'slime-repl-mode-hook 'override-slime-repl-bindings-with-paredit)
 
      ;; Ruby
      (global-rinari-mode)
