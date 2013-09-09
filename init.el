a2d673b2edd51b490939782b50db77b8bc312290
Yuri Albuquerque <yuridenommus@gmail.com>
2013-09-09 11:34:10 -0400

    Magit's new item highlight was not readable.

--- init.el
+++ init.el
@@ -33,7 +33,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(magit-item-highlight ((t (:background "black")))))
 
 ;; Misc
 (add-to-list 'load-path "~/.emacs.d/plugins")
