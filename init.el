b1dd858a67e485c73f0522f8f50e03e1c59fa40c
Yuri Albuquerque <yuridenommus@gmail.com>
2013-11-23 15:53:12 -0400

    Chrome is now called with google-chrome-stable.

--- init.el
+++ init.el
@@ -42,7 +42,7 @@
 (setq gnus-button-url 'browse-url-generic
       browse-url-generic-program (if (eq system-type 'windows-nt)
                                      "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe"
-                                   "google-chrome")
+                                   "google-chrome-stable")
       browse-url-browser-function gnus-button-url)
 (tool-bar-mode 0)
 (menu-bar-mode 0)
