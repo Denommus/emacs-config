63cd2bae8a97695943c7853715dcbf6b0e6558c0
Yuri Albuquerque <yuridenommus@gmail.com>
2014-02-11 12:30:16 -0400

    Using MesloLGM typeface.

--- init.el
+++ init.el
@@ -32,7 +32,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:font "DejaVu Sans Mono-10"))))
+ '(default ((t (:font "MesloLGM"))))
  '(magit-item-highlight ((t (:background "black"))) t))
 
 ;; Misc
