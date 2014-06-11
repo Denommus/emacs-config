6295fc7b90adce0618f88c24aceb33820866ca7d
Yuri Albuquerque <yuridenommus@gmail.com>
2014-06-11 14:41:55 -0400

    Visible bell is less intrusive

--- init.el
+++ init.el
@@ -38,6 +38,7 @@
  '(magit-item-highlight ((t (:background "black")))))
 
 ;; Misc
+(setq visible-bell 1)
 (add-to-list 'default-frame-alist
              '(font . "Anonymous Pro-11"))
 (add-to-list 'load-path "~/.emacs.d/plugins")
