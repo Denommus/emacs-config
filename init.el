6c460f154b079d19b07403768071a2481617bb9d
Yuri Albuquerque <yuridenommus@gmail.com>
2014-10-20 17:54:06 -0400

    Ignoring some unpleasant person.

--- init.el
+++ init.el
@@ -22,6 +22,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(erc-ignore-list (quote ("ihatehex")))
  '(erc-modules (quote (autojoin button completion fill irccontrols list log match menu move-to-prompt netsplit networks noncommands readonly ring scrolltobottom smiley stamp track truncate)))
  '(inhibit-startup-screen t)
  '(org-agenda-files (quote ("~/Dropbox/org/metas.org" "~/Dropbox/org/agenda.org" "~/Dropbox/org/mobile.org" "~/Dropbox/org/capture.org")))
@@ -35,7 +36,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((t (:height 110 :family "Anonymous Pro"))))
- '(magit-item-highlight ((t (:background "black")))))
+ '(magit-item-highlight ((t (:background "black"))) t))
 
 ;; Misc
 (setq visible-bell 1)
