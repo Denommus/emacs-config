15afe1f09749d69758cf166f5f4537ea1e05622f
Yuri Albuquerque <yuridenommus@gmail.com>
2018-05-29 17:42:57 -0300

    pdf-tools is MILES better than the default pdf viewer.

--- init.el
+++ init.el
@@ -535,6 +535,13 @@
   (add-hook 'slime-repl-mode-hook #'enable-smartparens-mode)
   (load-file "~/.emacs.d/smartparens.el"))
 
+;; PDFView mode
+(use-package pdf-tools
+  :config
+  (use-package pdf-view-mode
+    :mode
+    "\\.pdf\\'"))
+
 ;; RJSX-Mode
 (use-package rjsx-mode
   :mode
