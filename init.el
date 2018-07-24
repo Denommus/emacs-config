d70e0e885efe54ab2107109d6f4538eaa7592371
Yuri Albuquerque <yuridenommus@gmail.com>
2018-07-24 18:25:19 -0300

    I never use marmalade.

--- init.el
+++ init.el
@@ -22,7 +22,6 @@
 ;; Packages
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
                          ("melpa" . "http://melpa.milkbox.net/packages/")
-                         ("marmalade" . "http://marmalade-repo.org/packages/")
                          ("org" . "http://orgmode.org/elpa/")))
 
 ;; Added by Package.el.  This must come before configurations of
