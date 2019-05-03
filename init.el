afe5c2aee360475ac682d6c31fd43f7ad9621f5e
Yuri Albuquerque <yuridenommus@gmail.com>
2019-05-03 15:18:56 -0300

    SSH_AUTH_SOCK in exec-path-from-shell

--- init.el
+++ init.el
@@ -56,7 +56,7 @@
  '(org-agenda-files (quote ("~/Dropbox/org/agenda.org")))
  '(package-selected-packages
    (quote
-    (flycheck-rust mingus magit-popup nix-buffer typescript-mode forge lsp-clients org-ref visual-regexp csv-mode ox ox-latex org-git-link ox-taskjuggler merlin ocp-indent intero exec-path-from-shell bbdb helm-bbdb monokai-alt-theme birds-of-paradise-plus-theme web-mode php-mode pdf-tools hydra org-plus-contrib helm-mu lsp-ui reason-mode tuareg twittering-mode yasnippet discover mastodon emojify writegood-mode webpaste switch-window go gnugo omnisharp pkgbuild-mode edit-indirect rjsx-mode opam image+ virtualenvwrapper elfeed-org elfeed helm-company haml-mode helm-google multi-term gnuplot gnuplot-mode org-bullets ht plantuml-mode nginx-mode helm-flyspell helm-spotify-plus rust-mode use-package bbdb-android hc-zenburn-theme slime idris-mode dockerfile-mode exercism scala-mode markdown-mode markdown-mode+ htmlize tronesque-theme fsharp-mode editorconfig python-django multiple-cursors nix-mode feature-mode color-theme-solarized yaml-mode undo-tree toml-mode smartparens show-css ruby-block robe qml-mode org-mime magit-svn lua-mode helm-projectile gitconfig-mode ggtags elscreen cyberpunk-theme csharp-mode company-ghci cmake-mode clojure-mode bundler bind-key auctex)))
+    (reason-mode sudoku flycheck-rust mingus magit-popup nix-buffer typescript-mode forge lsp-clients org-ref visual-regexp csv-mode ox ox-latex org-git-link ox-taskjuggler merlin ocp-indent intero exec-path-from-shell bbdb helm-bbdb monokai-alt-theme birds-of-paradise-plus-theme web-mode php-mode pdf-tools hydra org-plus-contrib helm-mu lsp-ui tuareg twittering-mode yasnippet discover mastodon emojify writegood-mode webpaste switch-window go gnugo omnisharp pkgbuild-mode edit-indirect rjsx-mode opam image+ virtualenvwrapper elfeed-org elfeed helm-company haml-mode helm-google multi-term gnuplot gnuplot-mode org-bullets ht plantuml-mode nginx-mode helm-flyspell helm-spotify-plus rust-mode use-package bbdb-android hc-zenburn-theme slime idris-mode dockerfile-mode exercism scala-mode markdown-mode markdown-mode+ htmlize tronesque-theme fsharp-mode editorconfig python-django multiple-cursors nix-mode feature-mode color-theme-solarized yaml-mode undo-tree toml-mode smartparens show-css ruby-block robe qml-mode org-mime magit-svn lua-mode helm-projectile gitconfig-mode ggtags elscreen cyberpunk-theme csharp-mode company-ghci cmake-mode clojure-mode bundler bind-key auctex)))
  '(safe-local-variable-values
    (quote
     ((intero-targets "drawbridge-simulator:lib" "drawbridge-simulator:exe:drawbridge-simulator-exe" "drawbridge-simulator:test:drawbridge-simulator-test")
@@ -218,7 +218,6 @@
   (setq comint-password-prompt-regexp
         (concat comint-password-prompt-regexp
                 "\\|^\\[sudo\\]")))
-(setenv "SSH_AUTH_SOCK" (concat (getenv "XDG_RUNTIME_DIR") "/gnupg/S.gpg-agent.ssh"))
 (require 'cl-lib)
 (setq visible-bell 1)
 (add-to-list 'load-path "~/.emacs.d/plugins")
@@ -999,7 +998,7 @@
 (use-package exec-path-from-shell
   :init
   (exec-path-from-shell-initialize)
-  (exec-path-from-shell-copy-envs '("NIX_PATH" "MSBuildSDKsPath")))
+  (exec-path-from-shell-copy-envs '("NIX_PATH" "MSBuildSDKsPath" "SSH_AUTH_SOCK")))
 
 (use-package nix-mode
   :mode "\\.nix\\'")
